524ca52a7f59298187fd9011dc9117249dfcc870
AwsomeTheKingVic <126616653+AwsomeTheKingVic@users.noreply.github.com>
2023-11-15 09:22:20 -0700

    seconds half of infor

--- Content_Calculator/food_content_calculator/src/main.rs
+++ Content_Calculator/food_content_calculator/src/main.rs
@@ -3,13 +3,12 @@ use std::io;
 
 fn main() {
 	
+	/*
 	//holds the number of Ingredients and there content number
 	let mut ingredient_content: Vec<f64> = Vec::new();
 	let mut ingredient_content_error: Vec<f64> = Vec::new();
 	
-	
 	let mut test2: Vec<Vec<i8>> = Vec::new();
-	
 	let mut test3: Vec<i8> = vec![1,2,3,4];
 	
 	for i in 0..3{
@@ -30,11 +29,11 @@ fn main() {
 		
 	}
     
-	/*
 	println!("Press enter when done");
 	
 	loop{
 		
+		//user inputs ingredient amount
 		println!("Enter ingredients content ammount ");
 		
 		//holds the string from the user
@@ -54,6 +53,7 @@ fn main() {
 			
 		};
 		
+		//user inputs error
 		println!("Error range here: ");
 		
 		//holds the string from the user
@@ -74,18 +74,24 @@ fn main() {
 		};
 	
 	}
-
+	
+	//iterates through the vector printing out the index + 1, number, and error 
 	for (index, number) in ingredient_content.iter().enumerate(){
 		
 		println!("ingredient: {} = content amount of: {} mg +/- error of: {}", index + 1, number, ingredient_content_error[index]);
 		
 	}
+	*/
 	
-	let mut ingredient_proportion: Vec<f64> = Vec::new();
+//===============================================================================================================================================
+	
+	let mut food_slice: Vec<Vec<f64>> = Vec::new();
+	let mut food_proportion: Vec<f64> = Vec::new();
+	
+	println!("Press enter when done");
+	println!("Enter Slice information ");
 	
 	loop{
-		
-		println!("Enter ingredients content ammount ");
 		
 		//holds the string from the user
 		let mut input_proportion = String::new();
@@ -95,17 +101,60 @@ fn main() {
 		
 		match input_proportion.trim().parse() {
 				
-			Ok(num) => ingredient_proportion.push(num),
+			Ok(num) => food_proportion.push(num),
 			Err(_) => {
 				
-				break;
-			
+				food_slice.push(food_proportion.clone());
+				
+				//food_proportion.clear();
+				
+				//add another slice info
+				println!("Would you Like to add another(add) slice otherwise press enter");
+		
+				let mut exit = String::new();
+				
+				io::stdin().read_line(&mut exit).expect("Failed to read line");
+				
+				//continue the loop or break out
+				match exit.trim() == "add" {
+					
+					true => { 
+					
+						println!("Enter Slice information ");
+					
+						continue;
+						
+					}
+					
+					_ => break,
+					
+				}
+					
 			}
 			
 		};
 	
 	}
 	
-	*/
+	for i in food_slice.iter() {
+		
+		println!("Slice {}", i);
+		
+	}
+	
+	//print the vectors
+	for i in 0..food_slice.len() {
+		
+		println!("Slice {}", i + 1);
+		
+		for o in 0..food_proportion.len() {
+			
+			println!("  {}", food_slice[i][o]);
+			
+		}
+		
+		println!("");
+		
+	}
 	
 }
